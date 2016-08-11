84e42164508dc19f1f132c5fa8938de0553b15db
Alexey Stolpovsky <stolpovsky6@gmail.com>
2016-08-11 21:47:56 +0700

    broadcast

---
+++ udp_broadcast_server.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define SRV_PORT    3773
+#define SRV_ADDR    "192.168.2.255"
+#define MSG_MAXLEN  256
+
+int main()
+{
+	int                sockfd;
+	struct sockaddr_in servaddr;
+	char               buf[MSG_MAXLEN];
+    int                broadcastEnable;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sockfd == -1) {
+        perror("socket");
+        exit(-1);
+    }
+
+    broadcastEnable = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable)) == -1) {
+        perror("setsockopt");
+        exit(-1);
+    }
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	inet_pton(AF_INET, SRV_ADDR, &(servaddr.sin_addr));
+	servaddr.sin_port = htons(SRV_PORT);
+
+    strcpy(buf, "Hello");
+
+    while (1) {
+        printf("send message to client: %s\n", buf);
+        sleep(2);
+        sendto(sockfd, buf, strlen(buf)+1, 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    }
+
+	close(sockfd);
+	exit(0);
+}
---
+++ udp_client.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define SRV_PORT    3773
+#define SRV_ADDR    "192.168.2.255"
+#define MSG_MAXLEN  256
+
+int main()
+{
+	int                sockfd;
+	struct sockaddr_in servaddr;
+	char               msg[MSG_MAXLEN];
+	socklen_t          addr_size; 
+	int                msg_size;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sockfd == -1) {
+        perror("socket");
+        exit(-1);
+    }
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	inet_pton(AF_INET, SRV_ADDR, &(servaddr.sin_addr));
+	servaddr.sin_port = htons(SRV_PORT);
+
+	if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+    	perror("bind");
+    	exit(-1);
+    }
+
+    addr_size = sizeof(servaddr);
+
+	while (1) {
+		msg_size = recvfrom(sockfd, msg, MSG_MAXLEN, 0, (struct sockaddr *)&servaddr, &addr_size);
+		if (msg_size == -1) {
+			perror("recvfrom");
+	        exit(-1);
+		} else
+	        printf("%s\n", msg);
+    }
+
+    close(sockfd);
+	exit(0);
+}
